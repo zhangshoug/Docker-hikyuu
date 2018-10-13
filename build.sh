32b5ea1f75a8ebc907a95420da3a92d36196b39b
zhangshoug <39645806+zhangshoug@users.noreply.github.com>
2018-10-13 19:12:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@
 [ ! $buildName ] && buildName="hikyuu:lastest"
 
 #下载相关资源
-log4c="log4cplus-1.2.1-rc2.tar.gz"
-[ ! -f $log4c ] && wget -c -O $log4c https://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.2.1/log4cplus-1.2.1-rc2.tar.gz/download  &
+log4c="log4cplus-1.2.1.tar.gz"
+[ ! -f $log4c ] && wget -c -O $log4c https://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.2.1/log4cplus-1.2.1.tar.gz/download  &
 #[ ! -f hdf5-1.8.19.tar.gz ] && wget -c -O hdf5-1.8.19.tar.gz https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-1.8.19.tar.gz &
 [ ! -f ta-lib-0.4.0-src.tar.gz ] && wget -c -O ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz &
 [ ! -f boost_1_64_0.tar.gz ] && wget -c -O boost_1_64_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.64.0/boost_1_64_0.tar.gz/download &
 #[ ! -d hikyuu ] && git clone https://git.oschina.net/fasiondog/hikyuu.git &
-[ ! -d hikyuu ] && git clone https://github.com/fasiondog/hikyuu.git &
-[ -d hikyuu ] && echo "git pull hikyuu .........." && cd hikyuu && git pull && cd ..
+#[ ! -d hikyuu ] && git clone https://github.com/fasiondog/hikyuu.git &
+#[ -d hikyuu ] && echo "git pull hikyuu .........." && cd hikyuu && git pull && cd ..
 #[ ! -f sh/conda3.sh ] && wget -c -O sh/conda3.sh https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh && chmod a+x conda3.sh
-[ ! -f sh/conda3.sh ] && wget -c -O sh/conda3.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-4.4.0-Linux-x86_64.sh && chmod a+x conda3.sh
+[ ! -f sh/conda3.sh ] && wget -c -O sh/conda3.sh https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 
 [ -d archives ] && rm -rf lock partial
 [ ! -d archives ] && mkdir archives
