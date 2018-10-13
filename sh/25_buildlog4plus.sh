49ece6d28d01c7ad73877a11dcbc7a30a252d4d9
zhangshoug <39645806+zhangshoug@users.noreply.github.com>
2018-10-13 19:13:47 +0800

    Update 25_buildlog4plus.sh

--- sh/25_buildlog4plus.sh
+++ sh/25_buildlog4plus.sh
@@ -2,9 +2,9 @@
 echo start ................................. $0
 set -e
 
-log4c="log4cplus-1.2.1-rc2"
+log4c="log4cplus-1.2.1"
 log4cfile=$tmpdir/$log4c.tar.gz
-#[ ! -f $log4cfile ] && wget -c -O $log4cfile https://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.2.1/log4cplus-1.2.1-rc2.tar.gz/download
+#[ ! -f $log4cfile ] && wget -c -O $log4cfile https://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.2.1/log4cplus-1.2.1.tar.gz/download
 tar -xzf $log4cfile
 cd $log4c
 #./configure --prefix=/usr CXXFLAGS="std=-c++11"
